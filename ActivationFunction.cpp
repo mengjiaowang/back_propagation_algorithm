38494eed75bb130b61b7a645fc8d76cbf4f620dd
Mengjiao Wang <wmjdut@gmail.com>
2014-07-22 14:53:49 +0800

    add derivative forms of activation function

--- ActivationFunction.cpp
+++ ActivationFunction.cpp
@@ -11,9 +11,17 @@ double Sigmoid::operator()(int x){
     return 1/(1+exp(-1*x));
 }
 
+double Sigmoid::derivative(int x){
+    return x*(1-x);
+}
+
 Tanh::Tanh(){
 }
 
 double Tanh::operator()(int x){
     return (exp(x)-exp(-1*x))/(exp(x)+exp(-1*x));
+}
+
+double Tanh::derivative(int x){
+    return 1-(pow(exp(x)-exp(-1*x),2)/pow(exp(x)+exp(-1*x),2));
 }
--- ActivationFunction.h
+++ ActivationFunction.h
@@ -5,7 +5,7 @@ class ActivationFunction{
     public:
         ActivationFunction();
         virtual double operator()(int x) = 0;
-        //virtual double derivative(int x) = 0;
+        virtual double derivative(int x) = 0;
     private:
 };
 
@@ -13,14 +13,14 @@ class Sigmoid:public ActivationFunction{
     public:
         Sigmoid();
         double operator()(int x);
-        //double derivative(int x);
+        double derivative(int x);
 };
 
 class Tanh:public ActivationFunction{
     public:
         Tanh();
         double operator()(int x);
-        //double derivative(int x);
+        double derivative(int x);
 };
 
 #endif
