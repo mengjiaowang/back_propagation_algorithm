18f87af079d46e428bac2d586a476b66ebf23e0e
Mengjiao Wang <wmjdut@gmail.com>
2014-07-24 13:52:55 +0800

    add a new linear separable dataset

--- test.cpp
+++ test.cpp
@@ -8,6 +8,11 @@ using namespace std;
 
 void loadDataset(const char *path, vector<vector<double> > &dataset);
 void evaluation(NeuralNetwork &nn, vector<vector<double> > &testdata);
+int findMaximum(vector<double> & arr);
+
+int numRow = 0;
+int numLabels = 0;
+int numAttr = 0;
 
 int main(int argc, char* argv[]){
 
@@ -19,20 +24,21 @@ int main(int argc, char* argv[]){
     double learningRate = std::atof(argv[1]);
     double maxIteration = std::atoi(argv[2]);
 
+    // prepare training data
+    vector<vector<double> > dataset;
+//    string file = "datasets/wine.dat";
+    string file = "datasets/linear.dat";
+    loadDataset(file.c_str(), dataset);
+
     // define network structure
     Sigmoid sigmoid;
     Tanh tanh;
-    NeuralNetwork nn(13,200,3, sigmoid, tanh);
+    NeuralNetwork nn(numAttr, 100, numLabels, sigmoid, tanh);
     BackPropagation bp(nn);
-
-    // prepare training data
-    vector<vector<double> > dataset;
-    string file = "datasets/wine.dat";
-    loadDataset(file.c_str(), dataset);
 
     // normalization
     Normalization norm;
-    norm.normalize(dataset, 0, 12);
+    norm.normalize(dataset, 0, numAttr-1);
     vector<vector<double> > train(dataset.begin(), dataset.end());
     vector<vector<double> > test(dataset.begin(), dataset.end());
     
@@ -53,12 +59,11 @@ void evaluation(NeuralNetwork &nn, vector<vector<double> > &test){
 
     int correct = 0;
     for(int i = 0; i != test.size(); ++i){
-        vector<double> xValues(test[i].begin(), test[i].begin()+13);
-        vector<double> yValues(test[i].begin()+13, test[i].end());
+        vector<double> xValues(test[i].begin(), test[i].begin()+numAttr);
+        vector<double> yValues(test[i].begin()+numAttr, test[i].end());
         vector<double> &outputs = nn.computeOutputs(xValues);
-        if(outputs[0] > outputs[1] && outputs[0] > outputs[2] && yValues[0] == 1) correct ++;
-        if(outputs[1] > outputs[0] && outputs[1] > outputs[2] && yValues[1] == 1) correct ++;
-        if(outputs[2] > outputs[0] && outputs[2] > outputs[1] && yValues[2] == 1) correct ++;
+        int index = findMaximum(outputs);
+        if(yValues[index] == 1) correct ++;
     }
     cout << "Accuracy:" << (double)correct/test.size() << endl;
 }
@@ -66,20 +71,39 @@ void evaluation(NeuralNetwork &nn, vector<vector<double> > &test){
 void loadDataset(const char *path, vector<vector<double> > &dataset){
     fstream datafile;
     datafile.open (path, ios::in);
+    datafile >> numRow >> numLabels >> numAttr;
+
+    cout << "The number of instances: " << numRow << endl 
+         << "The number of lables: " << numLabels << endl
+         << "The number of attributes: " << numAttr << endl;
+
     while(!datafile.eof()){
         vector<double> row;
         dataset.push_back(row);
         double attribute = 0, label = 0;
         datafile >> label;
-        for(int i = 0; i != 13; i ++){
+        for(int i = 0; i != numAttr; i ++){
             datafile >> attribute;
             dataset.back().push_back(attribute);
         }
-        dataset.back().push_back(0);
-        dataset.back().push_back(0);
-        dataset.back().push_back(0);
-        dataset.back()[12+label] = 1;
+        for(int i = 0; i != numLabels; ++i){
+            dataset.back().push_back(0);
+        }
+        dataset.back()[numAttr+label-1] = 1;
     }
     datafile.close();
 
+}
+
+int findMaximum(vector<double> & arr){
+    if(arr.size() == 0) return -1;
+    double max = arr[0];
+    int index = 0;
+    for(int i = 1; i != arr.size(); ++i){
+        if(max < arr[i]){
+            max = arr[i];
+            index = i;
+        }
+    }
+    return index; 
 }
